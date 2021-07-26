ceedb4f3bc7dce766ff8955e51ab983cd06899d5
ThanhHuyenf <huyenmotnang@gmail.com>
2021-07-27 00:58:41 +0700

    fixed login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
-console.log("localStorage.login", localStorage)
+localStorage.setItem('admin1', JSON.stringify([{
+    name: ' huyen',
+    pass: '123'
+}]))
 if (localStorage.login == 'true') {
     document.getElementById('signin').style.display = 'none';
     document.getElementById('signout').style.display = 'block';
@@ -8,7 +11,6 @@ if (localStorage.login == 'true') {
 }
 
 function logout() {
-    localStorage.list = ''
     localStorage.login = false
     document.getElementById('backIndex').onclick()
 }
@@ -21,13 +23,18 @@ function submitlogin() {
     } else if (password === '') {
         alert('Vui lòng nhập mật khẩu')
     } else {
-        if (username == localStorage.username && password == localStorage.password) {
-            localStorage.login = true;
-            localStorage.list = JSON.stringify([]);
-            window.location = 'index.html'
+        if (checkAccount(username)) {
+            const temp = JSON.parse(localStorage.getItem(username))
+
+            if (temp[0].password === password) {
+                localStorage.login = true;
+                window.location = 'index.html'
+            }else {
+                alert('Mật khẩu không đúng')
+            }
         } else {
             localStorage.login = false;
-            alert('Sai tên tài khoản hoặc mật khẩu')
+            alert('Tài khoản không tồn tại')
         }
     }
 }
@@ -37,26 +44,40 @@ function statusLogin() {
 }
 
 function checkAccount(username) {
-    return localStorage.username != username
+    const check = localStorage.getItem(username)
+    //tài khoản có trong local sẽ trả về true, không tồn tại tài khoản sẽ trả về false
+    return check === null ? false : true
 }
 
-function checkSignin() {
-    var signUser = document.getElementsByClassName('sign__user')[0].value
-    var signPass = document.getElementsByClassName('sign__pass')[0].value
-    var signRepass = document.getElementsByClassName('sign__repass')[0].value
-    if (signUser === '') {
+function checkSignup() {
+    const signUser = document.getElementsByClassName('sign__user')[0].value
+    const signPass = document.getElementsByClassName('sign__pass')[0].value
+    const signRepass = document.getElementsByClassName('sign__repass')[0].value
+    const signAddress = document.getElementsByClassName('sign__address')[0].value
+    const signPhoneNumber = document.getElementsByClassName('sign__phoneNumber')[0].value
+
+    if (signUser.trim() === '') {
         alert('Tài khoản không hợp lệ')
     } else if (signPass === '') {
         alert('Vui lòng nhập mật khẩu')
     } else if (signRepass === '') {
         alert('Vui lòng nhập lại mật khẩu')
-    } else if (checkAccount(signUser)) {
-        if (signPass == signRepass) {
+    } else if(signAddress.trim() === ''){
+        alert('Vui lòng nhập lại địa chỉ')
+    }else if(signPhoneNumber.trim() === ''){
+        alert('Vui lòng nhập lại số điện thoại')
+    } else if (!checkAccount(signUser)) {
+        if (signPass === signRepass) {
             if (signPass.length < 6) {
                 alert('Mật khẩu chưa đủ độ dài tối thiểu! Vui lòng nhập lại')
             } else {
+                const newAccount = {
+                    password: signPass,
+                    address: signAddress,
+                    phoneNumber: signPhoneNumber
+                }
                 window.location = 'signupsuccess.html'
-                localStorage.setItem(signacc.value, signpass.value)
+                localStorage.setItem(signUser, JSON.stringify(newAccount))
             }
         } else {
             alert('Mật khẩu không trùng khớp')
